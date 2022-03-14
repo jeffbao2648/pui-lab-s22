e1f4a387a1cdc2715e252d8ecb5c8a98fbd8974e
jeffbao2648 <99672792+jeffbao2648@users.noreply.github.com>
2022-03-14 13:53:50 -0400

    Update main.js

--- week7_lab/main.js
+++ week7_lab/main.js
@@ -5,11 +5,11 @@ function Panda(name, age) {
   this.image = “panda.jpg”;
 }
 
-function Kowala(name, age) {
+function Koala(name, age) {
   this.name = name;
   this.age = age;
-  this.image_alt = “An image of a sleeping Kowala;
-  this.image = “kowala.jpg”;
+  this.image_alt = “An image of a sleeping Koala;
+  this.image = “koala.jpg”;
 }
 
 function Lion(name, age) {
@@ -19,5 +19,38 @@ function Lion(name, age) {
   this.image = “lion.jpg”;
 }
 
+var panda = new Panda("Hagrid", 2); 
+var koala = new Koala("Schleepy", 3); 
+var lion = new Lion("Boo", 5);
 
+const animals = [panda, koala, lion];
+const names = [panda.name, koala.name, lion.name];
 
+function generateRandomIndex(maxIndex) {
+  return Math.floor(Math.random() * maxIndex); 
+}
+
+function generateRandomName() {
+  var randomIndex = generateRandomIndex(maxIndex);
+  return names[randomIndex];
+}
+
+function generateRnadomAge() {
+  var randomIndex = generateRandomIndex(maxIndex); 
+  return names[randomIndex].age;
+}
+
+function generateRandomAnimal() {
+  var randomIndex = generateRandomIndex(maxIndex); 
+  var randomAnimal = animals[randomIndex];
+  var randomName = randomAnimal.name;
+  var randomAge = randomAnimal.age;
+  if (randomAnimal instanceof Panda) {
+    return new Panda(randomName, randomAge);
+  } else if (randomAnimal instance of Koala) {
+    return new Koala(randomName, randomAge); 
+  } else if (randomAnimal instnace of Lion) {
+    return new Lion(randomName, randomAge); 
+  };
+  
+}
