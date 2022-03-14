d3cec5bf955338694becc118def215660d561a8b
jeffbao2648 <99672792+jeffbao2648@users.noreply.github.com>
2022-03-14 10:49:07 -0400

    Update main.js

--- week7_lab/main.js
+++ week7_lab/main.js
@@ -2,5 +2,22 @@ function Panda(name, age) {
   this.name = name;
   this.age = age;
   this.image_alt = “An image of a Panda”;
-  this.image = “anImageOfMyAnimal.jpg”;
+  this.image = “panda.jpg”;
 }
+
+function Kowala(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image_alt = “An image of a sleeping Kowala;
+  this.image = “kowala.jpg”;
+}
+
+function Lion(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image_alt = “An image of a Lion”;
+  this.image = “lion.jpg”;
+}
+
+
+
