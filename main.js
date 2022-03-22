1b9a33fa27ef20c13aa971ae68091c399ab2b2f8
jeffbao2648 <99672792+jeffbao2648@users.noreply.github.com>
2022-03-22 18:54:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-let currCourses = [05-430, 70-257, 70-110, 15-110, 15-112];
-let currCourses = 0;
+let currCoursesID = [05-430, 70-257, 70-110, 15-110, 15-112];
+let currnNumCourses = 0;
+document.getElementById("num-courses") = currNumCourses;
 
 function addNewClass() {
-  document.getElementById("num-courses") = currCourses;
   let classInput = document.getElementById("text-input");
-  for (let i = 0; i<currCourses.length; i++) {
-    if (classInput.value == currCourses[i]) {
-      currCourses += 1;
-      document.getElementById("num-courses") = currCourses;
+  for (let i = 0; i<currCoursesID.length; i++) {
+    if (classInput.value == currCoursesID[i]) {
+      currNumCourses += 1;
+      document.getElementById("num-courses") = currNumCourses;
       alert("Class added!");
-      document.write(currCourses);
-      console.log(currCourses);
+      console.log(currNumCourses);
       return; 
     }
     alert("Invalid Class!");
