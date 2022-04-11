bfaa0c3ec08ac0496179ab8225abf84732063533
jeffbao2648 <99672792+jeffbao2648@users.noreply.github.com>
2022-04-11 16:34:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
-let currCoursesID = ['05-430', '70-257', '70-110', '15-110', '15-112'];
+function CourseInfo(ID, name, section, info, detail) {
+  this.ID = ID; 
+  this.name = name; 
+  this.section = section; 
+  this.info = info; 
+  this.extra_detail = detail;
+}
+
+
+let PUI = new CourseInfo("05-430", "Programmable User Interface", "Section: A and B", "Description: This course is combines lecture, and an intensive programming lab...", "Description: This course is combines lecture, and an intensive programming lab and design studio. It is for those who want to express their interactive ideas in working prototypes. It will cover the importance of human-computer interaction/interface design, iterative design, input/output techniques, how to design and evaluate interfaces, and research topics that will impact user interfaces in the future. In lab, you will learn how to design and program effective graphical user interfaces, and how to perform user tests. We will cover a number of prototyping tools and require prototypes to be constructed in each, ranging from animated mock-ups to fully functional programs. Assignments will require implementing UIs, testing that interface with users, and then modifying the interface based on findings. Some class sessions will feature design reviews of student work. This course is for HCII Masters students and HCI dual majors with a minimal programming background. Students will often not be professional programmers, but will need to interact with programmers.");
+
+let Opti = new CourseInfo("70-257", "Optimization for Business", "Section: A and B", "Description: This course provides a mathematical foundation for the application of optimization...", "This course provides a mathematical foundation for the application of optimization and analytic techniques to business problems, as well as the practical implementation of these methods. Mathematical optimization techniques have been applied for decades in the context of logistics, supply chain management, and strategic planning, with great success. In recent years, the application of mathematical optimization has penetrated, and in some cases (re-)defined, many other areas such as the (financial) service industry, analytical marketing, health care, and web-based businesses. In this course, the most important methods and techniques underlying mathematical optimization are studied. These include linear programming, integer programming, and nonlinear programming as basic mathematical methodologies. Based on these, we also consider methodologies for particular problem classes such as network models and traveling salesman problems. During the course we will emphasize mathematical modeling, that is, creating a mathematical description that reflects a given practical problem described in words. Motivated by these mathematical models, we then discuss the necessary mathematical techniques for finding optimal solutions. Lastly, we consider the solution of these problems using optimization software, i.e., we represent the mathematical models in Excel and use Excel Solver to compute an optimal solution.");
+
+let BusComp = new CourseInfo("70-110", "Business Computing", "Section: A and B", "Description: Students will learn how individuals and organizations use computing technologies...", "Description: Students will learn how individuals and organizations use computing technologies to support and improve their businesses. At an individual level, students will build their skills with Microsoft Excel and other personal productivity tools. At an organizational level, the class looks at ways in which businesses of all sizes and types leverage computing technologies to run their businesses more efficiently, make better business decisions, and create new business opportunities. This course is reserved for first-year Business students; others may enroll by special permission from the UBA office only.");
+
+let currCourses = [PUI, Opti, BusComp];
 let currRegCourses = [];
 let currNumCourses = 0;
 document.getElementById("num-courses").innerHTML = currNumCourses;
 
+function searchClass() {
+  let classInput = document.getElementById("search-input");
+  for (let i = 0; i<currCourses.length; i++) {
+    if (classInput.value == currCourses[i].ID) {
+      document.getElementById("searched-course-ID").textContent = currCourses[i].ID;
+      document.getElementById("searched-course-name").textContent = currCourses[i].name;
+      document.getElementById("searched-course-section").textContent = currCourses[i].section;
+      document.getElementById("searched-course-info").textContent = currCourses[i].info;
+      document.getElementById('addClass').style.display= 'block';
+      document.getElementById('dropClass').style.display= 'block';
+      document.getElementById('learnMore').style.display= 'block';
+    }
+  }
+}
+
 function addNewClass() {
-  let classInput = document.getElementById("text-input");
-  for (let i = 0; i<currCoursesID.length; i++) {
-    if (classInput.value == currCoursesID[i]) {
+  let classInput = document.getElementById("search-input");
+  for (let i = 0; i<currCourses.length; i++) {
+    if (classInput.value == currCourses[i].ID) {
       currNumCourses += 1;
       console.log(currNumCourses);
       document.getElementById("num-courses").innerHTML = currNumCourses;
@@ -20,7 +50,7 @@ function addNewClass() {
 }
 
 function dropClass() {
-  let classInput = document.getElementById("text-input");
+  let classInput = document.getElementById("search-input");
   for (let i = 0; i<currRegCourses.length; i++) {
     if (classInput.value == currRegCourses[i]){
       currNumCourses -= 1; 
@@ -35,13 +65,20 @@ function dropClass() {
   alert("Invalid Class!");
 }
 
-function CourseInfo(ID, name, section, info) {
-  this.ID = ID; 
-  this.name = name; 
-  this.section = section; 
-  this.info = info; 
+function learnMore() {
+  // very similar to searchClass
+  // using local storage instead
+  // localStorage.setItem --> set variable for index class 
+  // local.Storage.getItem --> gets the index of the searched class
+  
+  return; 
 }
 
-let PUI = new CourseInfo("05-430", "Programmable User Interface", "A and B", "Description: This course is combines lecture, and an intensive programming lab...")
+
+
+
+function onLoad() {
+  document.getElementById("searched-course").textContent = PUI.ID;
+}
 
 
